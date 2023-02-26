7395503c54c9a430aa3633388dab554fafae064d
David Oluseun <a.davidoluseun@gmail.com>
2023-02-26 21:35:18 +0100

    Use any temporarily

--- src/types/state.ts
+++ src/types/state.ts
@@ -12,5 +12,5 @@ export type AppState = {
 export type AppActionTypes =
   | { type: "SET_MANUALS"; payload: Manual[] }
   | { type: "SET_USER"; payload: User }
-  | { type: "SET_APP_DATA"; payload: AppData }
+  | { type: "SET_APP_DATA"; payload: any }
   | { type: "SET_MANUAL_PAYMENT"; payload: ManualPayInfo };
