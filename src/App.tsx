6519e3da5a4365a9529a4d8db77c61a79fb0f124
David Oluseun <a.davidoluseun@gmail.com>
2023-03-03 18:10:56 +0100

    Move some base configuration from main.tsx to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import * as React from "react";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { positions, Provider as AlertProvider } from "react-alert";
+import AlertTemplateMui from "react-alert-template-mui";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 import { Navigation } from "./navigation";
 import Loading from "./components/Loading";
 import Error from "./components/Error";
+import RouteScrollToTop from "./components/RouteScrollToTop";
 import { getUser } from "./auth/storage";
 import useAppApi from "./hooks/useAppApi";
+import theme from "./theme/theme";
+
+const alertOptions = {
+  position: positions.MIDDLE,
+};
 
 function App() {
   const user = getUser();
@@ -23,14 +32,18 @@ function App() {
   const handleTryAgain = () => request();
 
   return (
-    <>
-      {loading && user ? (
-        <Loading text="Please wait while we download your materials" />
-      ) : (
-        <>{error ? <Error onTryAgain={handleTryAgain} /> : <Navigation />}</>
-      )}
-      <ToastContainer position="bottom-left" hideProgressBar={true} />
-    </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AlertProvider template={AlertTemplateMui} {...alertOptions}>
+        <RouteScrollToTop />
+        {loading && user ? (
+          <Loading text="Please wait while we download your materials" />
+        ) : (
+          <>{error ? <Error onTryAgain={handleTryAgain} /> : <Navigation />}</>
+        )}
+        <ToastContainer position="bottom-left" hideProgressBar={true} />
+      </AlertProvider>
+    </ThemeProvider>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,35 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
-import { ThemeProvider, CssBaseline } from "@mui/material";
-import { positions, Provider as AlertProvider } from "react-alert";
-import AlertTemplateMui from "react-alert-template-mui";
 
 import AppProvider from "./state/AppProvider";
-import RouteScrollToTop from "./components/RouteScrollToTop";
-import theme from "./theme/theme";
 import App from "./App";
 import "./index.css";
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container as HTMLElement);
 
-const alertOptions = {
-  position: positions.MIDDLE,
-};
-
 root.render(
   <Router>
     <React.StrictMode>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <AlertProvider template={AlertTemplateMui} {...alertOptions}>
-          <RouteScrollToTop />
-          <AppProvider>
-            <App />
-          </AppProvider>
-        </AlertProvider>
-      </ThemeProvider>
+      <AppProvider>
+        <App />
+      </AppProvider>
     </React.StrictMode>
   </Router>
 );
