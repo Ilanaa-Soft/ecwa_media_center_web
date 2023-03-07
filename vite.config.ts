4a82e634afa944df7935d2d697ebb74d910cb549
Kayode Olaniyi <ifedamilola2009@gmail.com>
2023-03-07 15:59:26 +0100

    added pwa hopefully it works

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,69 @@
 import { defineConfig } from 'vite'
+import { ManifestOptions, VitePWA, VitePWAOptions } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react-swc'
+
+const pwaOptions: Partial<VitePWAOptions> = {
+  mode: 'production',
+  base: '/',
+  includeAssets: ['favicon.svg'],
+  manifest: {
+    name: 'Ecwa Media Center',
+    short_name: 'EMC',
+    theme_color: '#ffffff',
+    icons: [
+      {
+        src: './icons/icon-192x192.png', // <== don't add slash, for testing
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: './icons/icon-512x512.png', // <== don't remove slash, for testing
+        sizes: '512x512',
+        type: 'image/png',
+      },
+      {
+        src: './icons/icon-512x512.png', // <== don't add slash, for testing
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any maskable',
+      },
+    ],
+  },
+  devOptions: {
+    enabled: process.env.SW_DEV === 'true',
+    /* when using generateSW the PWA plugin will switch to classic */
+    type: 'module',
+    navigateFallback: 'index.html',
+  },
+}
+
+const replaceOptions = { __DATE__: new Date().toISOString() }
+const claims = process.env.CLAIMS === 'true'
+const reload = process.env.RELOAD_SW === 'true'
+const selfDestroying = process.env.SW_DESTROY === 'true'
+
+if (process.env.SW === 'true') {
+  pwaOptions.srcDir = 'src'
+  pwaOptions.filename = claims ? 'claims-sw.ts' : 'prompt-sw.ts'
+  pwaOptions.strategies = 'injectManifest'
+    ; (pwaOptions.manifest as Partial<ManifestOptions>).name = 'Ecwa Media Center'
+    ; (pwaOptions.manifest as Partial<ManifestOptions>).short_name = 'EMC'
+}
+
+if (claims)
+  pwaOptions.registerType = 'autoUpdate'
+
+if (reload) {
+  // @ts-expect-error overrides
+  replaceOptions.__RELOAD_SW__ = 'true'
+}
+
+if (selfDestroying)
+  pwaOptions.selfDestroying = selfDestroying
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(pwaOptions)],
 })
+
