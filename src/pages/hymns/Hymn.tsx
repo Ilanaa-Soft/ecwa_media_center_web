edf4c5098c0f533335361e7eadb370178dc78047
A. David Oluseun <53796675+davidoluseun@users.noreply.github.com>
2023-03-10 21:37:18 +0100

    Truncate hymn title and extra text

--- src/pages/hymns/Hymn.tsx
+++ src/pages/hymns/Hymn.tsx
@@ -1,4 +1,5 @@
 import { Typography, Box } from "@mui/material";
+import { makeStyles } from "@mui/styles";
 
 import { Hymn as HymnType } from "../../types";
 
@@ -7,7 +8,19 @@ type HymnProps = {
   onPushToHymn: (id: number) => void;
 };
 
+const useStyles = makeStyles(() => ({
+  truncate: {
+    display: "-webkit-box",
+    "-webkit-box-orient": "vertical",
+    "-webkit-line-clamp": 1,
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+  },
+}));
+
 const Hymn = ({ hymn, onPushToHymn }: HymnProps) => {
+  const classes = useStyles();
+
   return (
     <Box
       py="4px"
@@ -15,10 +28,17 @@ const Hymn = ({ hymn, onPushToHymn }: HymnProps) => {
       sx={{ cursor: "pointer" }}
       onClick={() => onPushToHymn(hymn.id)}
     >
-      <Typography textTransform="capitalize" fontWeight="500" component="h2">
+      <Typography
+        component="h2"
+        fontWeight="500"
+        textTransform="capitalize"
+        className={classes.truncate}
+      >
         {hymn.title}
       </Typography>
-      <Typography textTransform="capitalize">{hymn.extra}</Typography>
+      <Typography className={classes.truncate} textTransform="capitalize">
+        {hymn.extra}
+      </Typography>
     </Box>
   );
 };
